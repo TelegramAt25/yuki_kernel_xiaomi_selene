c2ff3a1ba9aceb062c167b2c8f46fe230c154f0b
Carl Fang <carl.fang@mediatek.com>
2021-04-17 15:37:04 +0800

    [ALPS05552516] ccu: fix mva abnormal in dual cam
    fix mva abnormal in dual cam
    
    MTK-Commit-Id: 52e1f78c0bc9f8bf7787c1e95c10e246fc3e76ec
    
    Change-Id: I951977b89e67fc42b9c6da787487adb61e1a6000
    CR-Id: ALPS05552516
    Feature: Camera Control Unit(ccu)
    Signed-off-by: Carl Fang <carl.fang@mediatek.com>

--- drivers/misc/mediatek/ccu/src/2.0/ccu_i2c.c
+++ drivers/misc/mediatek/ccu/src/2.0/ccu_i2c.c
@@ -268,6 +268,7 @@ int ccu_get_i2c_dma_buf_addr(struct ccu_i2c_buf_mva_ioarg *ioarg)
 
 	/*If there is existing i2c buffer mva allocated, deallocate it first*/
 	ccu_deallocate_mva(i2c_mva[ioarg->sensor_idx]);
+	i2c_mva[ioarg->sensor_idx] = 0;
 	ret = ccu_allocate_mva(&i2c_mva[ioarg->sensor_idx], va, 4096);
 	ioarg->mva = i2c_mva[ioarg->sensor_idx];
 	return ret;
@@ -279,8 +280,10 @@ int ccu_i2c_free_dma_buf_mva_all(void)
 	uint32_t i;
 
 	for (i = IMGSENSOR_SENSOR_IDX_MIN_NUM;
-		i < IMGSENSOR_SENSOR_IDX_MAX_NUM; i++)
+		i < IMGSENSOR_SENSOR_IDX_MAX_NUM; i++) {
 		ccu_deallocate_mva(i2c_mva[i]);
+		i2c_mva[i] = 0;
+	}
 
 	LOG_INF_MUST("%s done.\n", __func__);
 
