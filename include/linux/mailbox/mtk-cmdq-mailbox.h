3de7a8dd77828716daa7900e5095ee1e498d080c
Kartik-91 <kartikbanwalaop@gmail.com>
2023-11-21 16:27:06 +0000

    include: Import OEM Changes
    Signed-off-by: Kartik-91 <kartikbanwalaop@gmail.com>

--- include/linux/mailbox/mtk-cmdq-mailbox.h
+++ include/linux/mailbox/mtk-cmdq-mailbox.h
@@ -14,6 +14,8 @@
 #if !IS_ENABLED(CONFIG_MTK_CMDQ_MBOX_EXT)
 #define cmdq_util_msg(f, args...) cmdq_msg(f, ##args)
 #define cmdq_util_err(f, args...) cmdq_dump(f, ##args)
+#define cmdq_util_user_msg(chan, f, args...) cmdq_util_msg(f, ##args)
+#define cmdq_util_user_err(chan, f, args...) cmdq_util_err(f, ##args)
 #endif
 
 /* see also gce platform binding header */
@@ -142,6 +144,10 @@ struct cmdq_pkt {
 #endif
 #endif	/* end of CONFIG_MTK_CMDQ_MBOX_EXT */
 	bool			task_alloc;
+#if defined(CONFIG_MTK_MT6382_BDG)
+	void			*bdg_data;
+	bool			reuse;
+#endif
 };
 
 struct cmdq_thread {
@@ -224,8 +230,6 @@ void cmdq_thread_set_spr(struct mbox_chan *chan, u8 id, u32 val);
 void cmdq_init_cmds(void *dev_cmdq);
 void cmdq_mbox_channel_stop(struct mbox_chan *chan);
 void cmdq_dump_core(struct mbox_chan *chan);
-void cmdq_pkt_poll_gpr_check(struct cmdq_pkt *pkt,
-	const u16 gpr_idx, const s32 start);
 void cmdq_thread_dump_spr(struct cmdq_thread *thread);
 void cmdq_thread_dump(struct mbox_chan *chan, struct cmdq_pkt *cl_pkt,
 	u64 **inst_out, dma_addr_t *pc_out);
