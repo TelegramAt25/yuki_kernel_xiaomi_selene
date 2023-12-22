e151ba22a1aada9e438a6ab776b7b84da310642d
fukiame <fukiame@proton.me>
2023-12-23 05:53:23 +0700

    [DNM]include: mtk-cmdq-mailbox: sile

--- include/linux/mailbox/mtk-cmdq-mailbox.h
+++ include/linux/mailbox/mtk-cmdq-mailbox.h
@@ -169,7 +169,7 @@ extern int mtk_cmdq_log;
 #define cmdq_log(fmt, args...) \
 do { \
 	if (mtk_cmdq_log) \
-		pr_notice("[cmdq] "fmt" @%s,%u\n", \
+		pr_debug("[cmdq] "fmt" @%s,%u\n", \
 		##args, __func__, __LINE__); \
 } while (0)
 
@@ -177,13 +177,13 @@ do { \
 /* MTK only functions */
 
 #define cmdq_msg(fmt, args...) \
-	pr_notice("[cmdq] "fmt"\n", ##args)
+	pr_debug("[cmdq] "fmt"\n", ##args)
 
 #define cmdq_err(fmt, args...) \
 	pr_notice("[cmdq][err] "fmt" @%s,%u\n", ##args, __func__, __LINE__)
 
 #define cmdq_dump(fmt, args...) \
-	pr_notice("[cmdq][err] "fmt"\n", ##args)
+	pr_debug("[cmdq][err] "fmt"\n", ##args)
 
 /* CMDQ FTRACE */
 #define cmdq_trace_begin(fmt, args...) do { \
